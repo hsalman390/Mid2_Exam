a1325b5bea12df70303f8013ec1122ce38c27612
hsalman390 <hsalman390@gmail.com>
2020-09-12 17:07:53 +0500

    Mid2 Exams First Commit

---
+++ StudentMid2/Controllers/StudentModelsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentMid2.Models;
+
+namespace StudentMid2.Controllers
+{
+    public class StudentModelsController : Controller
+    {
+        private StudentDBContext db = new StudentDBContext();
+
+        // GET: StudentModels
+        public ActionResult Index(string studentGen, string searchString)
+        {
+            var StudentModel = new List<string>();
+
+            var GenderQry = from d in db.Student
+                           orderby d.Gender
+                           select d.Gender;
+
+            StudentModel.AddRange(GenderQry.Distinct());
+            ViewBag.studentGen = new SelectList(StudentModel);
+
+            var students = from m in db.Student
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.Name.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(studentGen))
+            {
+                students = students.Where(x => x.Gender == studentGen);
+            }
+
+            return View(students);
+        }
+
+        // GET: StudentModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentModel studentModel = db.Student.Find(id);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // GET: StudentModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,DateOfBirth,Gender,Fee")] StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Student.Add(studentModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentModel);
+        }
+
+        // GET: StudentModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentModel studentModel = db.Student.Find(id);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // POST: StudentModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,DateOfBirth,Gender,Fee")] StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentModel);
+        }
+
+        // GET: StudentModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentModel studentModel = db.Student.Find(id);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // POST: StudentModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentModel studentModel = db.Student.Find(id);
+            db.Student.Remove(studentModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StudentMid2/Models/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StudentMid2.Models
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public decimal Fee { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<StudentModel> Student { get; set; }
+    }
+}
